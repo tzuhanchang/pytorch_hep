f44448dc5274be6da22678393ebf6c6c6bf28642
tzuhanchang <han.tzu.zhang@gmail.com>
2022-06-11 02:01:56 +0100

    Bug fixing

--- torch_hep/_lorentz.py
+++ torch_hep/_lorentz.py
@@ -6,49 +6,56 @@ from torch_scatter import scatter
 from pylorentz import Momentum4
 
 
-class LorentzTensor():
+class LorentzTensor(object):
+    r"""Convert a `torch.Tensor` with size `torch.Size([N,4])` into a
+    `LorentzTensor`. Each `dim=0` is a 4 vector, `torch.Tensor` allows
+    high performance Lorentz vector manipulation over entire vector space,
+    it also enables GPU computation.
+
+    Args:
+        input (torch.tensor): input tensor with size `torch.Size([N,4])`.
+    """
     def __init__(self, input: Tensor):
         if input.ndim == 1:
             if input.size() == torch.Size([4]):
                 self.values = input
-                self.device = input.get_device()
+                self.device = input.device
                 self.type = 'single'
+                self.size = 1
             else:
                 raise TypeError("For 1 dimension tensor, expect size 4.")
         else:
             if input.size(dim=1) == 4:
                 self.values = input
-                self.device = input.get_device()
+                self.device = input.device
                 self.type = 'long'
+                self.size = self.values.size(dim=0)
             else:
                 raise TypeError("Expect size 4 in dimension 1.")
 
-    def size(self, dim: int=0):
-        if self.type == 'long':
-            return self.values.size(dim)
-        elif self.type == 'single':
-            return 1
-
     def __add__(self, other):
-        return __class__(torch.add(self.values, other.values))
+        return self.__class__(torch.add(self.values, other.values))
 
     def __sub__(self, other):
-        return __class__(torch.sub(self.values, other.values))
+        return self.__class__(torch.sub(self.values, other.values))
 
     def __mul__(self, other):
         if hasattr(other, "__len__") == False and hasattr(other, "size") == False:
-            return torch.multiply(other,self.values)
+            return __class__(torch.multiply(other,self.values))
         elif (other.values).size() == (self.values).size():
-            indices = torch.arange(0,self.size(),1,device='cuda').expand(4,self.size()).transpose(0,1).reshape(1,4*self.size()).to(torch.long)
-            METRIC = torch.diag(torch.tensor([1,-1,-1,-1],device='cuda').repeat(1,self.size())[0])
-            scattered = torch.diagonal(METRIC*self.values.reshape(1,4*self.size())*other.values.reshape(1,4*self.size()),0)
-            return scatter(scattered,index=indices[0],reduce='sum')
+            indices = torch.arange(0,self.size,1,device=self.device,dtype=torch.int64).expand(4,self.size).transpose(0,1).reshape(1,4*self.size)
+            METRIC = torch.diag(torch.tensor([1,-1,-1,-1],device=self.device).repeat(1,self.size)[0])
+            scattered = torch.diagonal(METRIC*self.values.reshape(1,4*self.size)*other.values.reshape(1,4*self.size),0)
+            return scatter(scattered,index=indices[0],reduce='sum').reshape(self.size,1)
         else:
             raise TypeError("Expect two Lorentz Tensor are same size.")
     
+    def __rmul__(self, other):
+        return self * other
+    
     def __truediv__(self, other):
         if hasattr(other, "__len__") == False and hasattr(other, "size") == False:
-            return 
+            return __class__(torch.div(self.values,other,rounding_mode='floor'))
         else:
             raise TypeError("Division only possible by scalars.")
     
@@ -85,44 +92,42 @@ class LorentzTensor():
     
     @property
     def trans(self):
-        return torch.sqrt(self.values.select(1,1)**2+self.values.select(1,2)**2)
+        return torch.sqrt(self.values.select(1,1)**2+self.values.select(1,2)**2).reshape(self.size,1)
 
     @property
     def phi(self):
-        return torch.arctan2(self.values.select(1,2),self.values.select(1,1))
+        return torch.atan2(self.values.select(1,2),self.values.select(1,1)).reshape(self.size,1)
         
     @property
     def theta(self):
-        return torch.arctan2(self.trans,self.values.select(1,3))
+        return torch.atan2(self.trans,self.values.select(1,3)).reshape(self.size,1)
+
+    @property
+    def eta(self):
+        return -torch.log(torch.tan(self.theta/ 2)).reshape(self.size,1)
 
 
+# class MomentumTensor(LorentzTensor):
+#     @staticmethod
+#     def EEtaPhiPt(e_eta_phi_pt: Tensor):
+#         px = torch.cos(e_eta_phi_pt.select(-1,2)) * 
+#         return MomentumTensor(torch.cat(()))
 
-a = LorentzTensor(torch.tensor([[1,1,1,1],[2,2,2,2],[3,3,3,3]],device='cuda'))
-b = LorentzTensor(torch.tensor([[3,3,3,3],[2,1,2,3],[2,1,2,3]],device='cuda'))
-c = LorentzTensor(torch.tensor([3,3,3,3],device='cuda'))
-d = LorentzTensor(torch.tensor([2,1,2,3],device='cuda'))
-a_1_m = Momentum4(1,1,1,1)
-a_2_m = Momentum4(2,2,2,2)
-b_1_m = Momentum4(3,3,3,3)
-b_2_m = Momentum4(2,1,2,3)
-print(b.theta)
-print(b_2_m.theta)
 
-# print((b*3))
-# print(b_1_m*3)
-# print(b_2_m*3)
-# print(torch.range(0,1,1).expand(4,2).transpose(0,1).reshape(1,8))
-# print(torch.range(0,1,1).expand(4*8,2).transpose(0,1).reshape(8,8))
-# lenth = torch.arange(0,2,1,device='cuda').expand(4*8,2).transpose(0,1).reshape(8,8).to(torch.long)
-# lenth2 =torch.arange(0,2,1,device='cuda').expand(4,2).transpose(0,1).reshape(1,8).to(torch.long)
-# from torch_scatter import scatter, segment_coo, segment_csr
-# b=torch.tensor([[3,3,3,3],[2,1,2,3]],device='cuda').reshape(1,8)
-# a=torch.tensor([[1,1,1,1],[2,2,2,2]],device='cuda').reshape(1,8)
-# print(torch.tensor([1,-1,-1,-1],device='cuda').repeat(1,2)[0])
-# print(torch.diagonal(scatter(segment_coo(torch.diag(torch.tensor([1,-1,-1,-1],device='cuda').repeat(1,2)[0])*b*a,
-#               index=lenth2[0],
-#               reduce='sum'),
-#               index=lenth2[0],
-#               reduce='sum')),0)
-# diaa = torch.diagonal((torch.diag(torch.tensor([1,-1,-1,-1],device='cuda').repeat(1,2)[0])*b*a),0)
-# print(scatter(diaa,index=lenth2[0],reduce='sum'))
+"""
+TEST
+"""
+b1 = Momentum4(111549, 35202.7, -46507.4, 94552.1)
+b2 = Momentum4(86549.2, 12443.8, 81453.1, 25407.5)
+lep_pos = Momentum4(96806.2, 29784.7, -14348.2, 90985.8)
+lep_neg = Momentum4(27209.1, 26173.4, 3376.81, 6623.67)
+met_met = 39915.24609375
+met_phi = -1.7860441207885742
+
+print(lep_pos.mag2, lep_neg.mag2)
+
+b = LorentzTensor(torch.tensor([[111549, 35202.7, -46507.4, 94552.1],[86549.2, 12443.8, 81453.1, 25407.5]],device='cpu',dtype=torch.float64))
+lep = LorentzTensor(torch.tensor([[96806.2, 29784.7, -14348.2, 90985.8],[27209.1, 26173.4, 3376.81, 6623.67]],device='cpu',dtype=torch.float64))
+
+print((lep).mag2)
+
