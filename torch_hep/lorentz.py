0f0e97076330cfed5df93d4c1452ca177016403c
tzuhanchang <han.tzu.zhang@gmail.com>
2022-06-13 17:21:32 +0100

    Bug fixing

--- torch_hep/lorentz.py
+++ torch_hep/lorentz.py
@@ -1,7 +1,6 @@
 import torch
 
 from torch import Tensor
-from torch_scatter import scatter
 
 
 class LorentzTensor(object):
@@ -37,13 +36,8 @@ class LorentzTensor(object):
     def __mul__(self, other):
         if hasattr(other, "__len__") == False and hasattr(other, "size") == False:
             return __class__(torch.multiply(other,self.values))
-        elif (other.values).size() == (self.values).size():
-            indices = torch.arange(0,self.size,1,device=self.device,dtype=torch.int64).expand(4,self.size).transpose(0,1).reshape(1,4*self.size)
-            METRIC = torch.diag(torch.tensor([1,-1,-1,-1],device=self.device).repeat(1,self.size)[0])
-            scattered = torch.diagonal(METRIC*self.values.reshape(1,4*self.size)*other.values.reshape(1,4*self.size),0)
-            return scatter(scattered,index=indices[0],reduce='sum').reshape(-1,1)
         else:
-            raise TypeError("Expect two Lorentz Tensor are same size.")
+            raise TypeError("'__mul__' only possible with a scalar")
     
     def __rmul__(self, other):
         return self * other
@@ -52,13 +46,13 @@ class LorentzTensor(object):
         if hasattr(other, "__len__") == False and hasattr(other, "size") == False:
             return __class__(torch.div(self.values,other,rounding_mode='floor'))
         else:
-            raise TypeError("Division only possible by scalars.")
+            raise TypeError("'__div__' only possible with a scalar")
     
     def __floordiv__(self, other):
         if hasattr(other, "__len__") == False and hasattr(other, "size") == False:
             return __class__(torch.div(self.values,other,rounding_mode='floor'))
         else:
-            raise TypeError("Floor division only possible by scalars.")
+            raise TypeError("'__floordiv__' only possible with a scalar")
     
     def __div__(self, other):
         if hasattr(other, "__len__") == False and hasattr(other, "size") == False:
@@ -68,8 +62,8 @@ class LorentzTensor(object):
         return self.values.select(-1,i).reshape((-1,1))
 
     def dot(self, other):
-        if (other.values).size() == torch.Size([4]):
-            return self*other
+        if (other.values).size() == (self.values).size():
+            return torch.diagonal(torch.tensordot(self.values,torch.mul(torch.tensor([1,-1,-1,-1],device=self.device),other.values),dims=([1],[1]))).reshape(-1,1)
 
     def to_tensor(self):
         return self.values
